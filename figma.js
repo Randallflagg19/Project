4dd925113a120f03bf05c953b6b8b07f2518f3bc
Randallflagg19 <121073882+Randallflagg19@users.noreply.github.com>
2024-03-21 07:25:23 +0300

    chore: addSetTasks1

--- figma.js
+++ figma.js
@@ -1,71 +1,101 @@
-const moviesList = [
-  {
-    id: "1",
-    title: "Dune",
-    status: true,
-  },
-  {
-    id: "2",
-    title: "The Shawshank Redemption",
-    status: false,
-  },
-  {
-    id: "3",
-    title: "The Matrix",
-    status: false,
-  },
-  // {
-  //   id: "4",
-  //   title: "The Godfather",
-  //   status: true,
-  // },
-  // {
-  //   id: "5",
-  //   title: "The Dark Knight",
-  //   status: true,
-  // },
-  // {
-  //   id: "6",
-  //   title: "Schindler's List",
-  //   status: false,
-  // },
-  // {
-  //   id: "7",
-  //   title: "The Lord of the Rings: The Return of the King",
-  //   status: true,
-  // },
-  // {
-  //   id: "8",
-  //   title: "The Shawshank Redemption",
-  //   status: true,
-  // },
-  // {
-  //   id: "9",
-  //   title: "Pulp Fiction",
-  //   status: false,
-  // },
-  // {
-  //   id: "10",
-  //   title: "Forrest Gump",
-  //   status: true,
-  // },
-];
+// const rawMoviesList = [
+//   {
+//     id: "1",
+//     title: "Dune",
+//     status: true,
+//   },
+//   {
+//     id: "2",
+//     title: "The Shawshank Redemption",
+//     status: false,
+//   },
+//   {
+//     id: "3",
+//     title: "The Matrix",
+//     status: false,
+//   },
+//   {
+//     id: "4",
+//     title: "The Godfather",
+//     status: true,
+//   },
+//   {
+//     id: "5",
+//     title: "The Dark Knight",
+//     status: true,
+//   },
+//   // {
+//   //   id: "6",
+//   //   title: "Schindler's List",
+//   //   status: false,
+//   // },
+//   // {
+//   //   id: "7",
+//   //   title: "The Lord of the Rings: The Return of the King",
+//   //   status: true,
+//   // },
+//   // {
+//   //   id: "8",
+//   //   title: "The Shawshank Redemption",
+//   //   status: true,
+//   // },
+//   // {
+//   //   id: "9",
+//   //   title: "Pulp Fiction",
+//   //   status: false,
+//   // },
+//   // {
+//   //   id: "10",
+//   //   title: "Forrest Gump",
+//   //   status: true,
+//   // },
+// ];
 
-// localStorage.setItem("tasks", JSON.stringify(moviesList3));
-//
-// const moviesList = moviesList1.filter((item) => (item.id = "3"));
+// localStorage.setItem("tasks", JSON.stringify(rawMoviesList));
 
-// const moviesList = JSON.parse(localStorage.getItem("tasks")) || [];
+let moviesList = JSON.parse(localStorage.getItem("tasks")) || [];
 
 const globalTasks = document.querySelector(".thirdLine");
 const inp = document.querySelector(".input");
 const circleButton = document.querySelector(".circle");
 
+const doneButton = document.querySelector(".done");
+const notDoneButton = document.querySelector(".notDone");
+const allButton = document.querySelector(".all");
 
+doneButton.addEventListener("click", (e) => {
+  while (globalTasks.firstChild) {
+    globalTasks.removeChild(globalTasks.firstChild);
+  }
+  let moviesListDone = moviesList.filter((item) => item.status);
+  moviesListDone.forEach((movie) => {
+    let createNewMovie = createTask(movie.title, movie.id, movie.status);
+    globalTasks.append(createNewMovie);
+  });
+});
 
+notDoneButton.addEventListener("click", (e) => {
+  while (globalTasks.firstChild) {
+    globalTasks.removeChild(globalTasks.firstChild);
+  }
+  let moviesListDone = moviesList.filter((item) => !item.status);
+  moviesListDone.forEach((movie) => {
+    let createNewMovie = createTask(movie.title, movie.id, movie.status);
+    globalTasks.append(createNewMovie);
+  });
+});
 
+allButton.addEventListener("click", (e) => {
+  while (globalTasks.firstChild) {
+    globalTasks.removeChild(globalTasks.firstChild);
+  }
+  moviesList.forEach((movie) => {
+    let createNewMovie = createTask(movie.title, movie.id, movie.status);
+    globalTasks.append(createNewMovie);
+  });
+});
 
-function createTask(task, status) {
+function createTask(task, id, status) {
   const newElement = document.createElement("div");
   newElement.innerHTML = `<div class="task">
     <div class="leftinfo">
@@ -80,109 +110,85 @@ function createTask(task, status) {
     </div>
   </div>`;
 
-function taskAddEventListeners(newElement, status = false){
-  const newCheckBox = newElement.querySelector(".bar");
-  const newTodoText = newElement.querySelector(".todo-text");
-  const newTrashBtn = newElement.querySelector(".trash");
-  
-  newCheckBox.checked = status;
-  
-  if (newCheckBox.checked) {
-    newTodoText.classList.add("todo-text_through");
-  }
-    
-  newCheckBox.addEventListener("change", (e) => {
-    if (status) {
-      e.target.checked;
-    }
-    
-    if (e.target.checked) {
+  function taskAddEventListeners(newElement, id, status = false) {
+    const newCheckBox = newElement.querySelector(".bar");
+    const newTodoText = newElement.querySelector(".todo-text");
+    const newTrashBtn = newElement.querySelector(".trash");
+    let newId = id
+    newCheckBox.checked = status;
+
+    if (newCheckBox.checked) {
       newTodoText.classList.add("todo-text_through");
-    } else {
-      newTodoText.classList.remove("todo-text_through");
     }
-  });
 
-  newTrashBtn.addEventListener("click", (e) => removeTask(e));
+    newCheckBox.addEventListener("change", (e) => {
+      if (status) {
+        e.target.checked
+      }
 
-  function removeTask(e) {
-    const taskEl = e.target.closest(".task");
-  
-    if (taskEl) {
-      taskEl.remove();
+      if (e.target.checked) {
+        newTodoText.classList.add("todo-text_through");
+      } else {
+        newTodoText.classList.remove("todo-text_through")
+      }
+    });
+
+    newTrashBtn.addEventListener("click", (e) => removeTask(e));
+
+    function removeTask(e) {
+      const taskEl = e.target.closest(".task");
+
+      if (taskEl) {        
+        moviesList = moviesList.filter((item) => item.id !== newId)        
+        taskEl.remove();        
+        localStorage.setItem("tasks", JSON.stringify(moviesList));
+      }
     }
   }
-}
 
-taskAddEventListeners(newElement, status)
-
+  taskAddEventListeners(newElement, id, status);
   return newElement;
 }
 
-
-
 moviesList.forEach((movie) => {
-  let createNewMovie = createTask(movie.title, movie.status);
+  let createNewMovie = createTask(movie.title, movie.id, movie.status);
   globalTasks.append(createNewMovie);
 });
 
-
-
-
-//ewe
-
-
-// circleButton.addEventListener("click", (e) => {
-//   const inputValue = inp.value;
-//   if (inputValue == ''){
-//     alert('Input is empty')
-//   }
-//   else{
-//   const createNewTask = createTask(inputValue);
-//   globalTasks.append(createNewTask)
-//   }
-
-//   // moviesListz
-
-//   // moviesList.push({ id: "11", title: inp.value, status: false });
-
-//   // localStorage.setItem("tasks", JSON.stringify(moviesList));
-//   // console.log(moviesList);
-
-//   inp.value = "";
-// });
-
-
-
 circleButton.addEventListener("click", (e) => {
   const inputValue = inp.value;
-  if (inputValue == ''){
-    alert('Input is empty')
-  }
-  else{
-  const createNewTask = createTask(inputValue);
-  globalTasks.append(createNewTask)
-  }
+  if (inputValue == "") {
+    alert("Input is empty");
+  } else {
+    const createNewTask = createTask(inputValue, globalTasks.children.length);
+    globalTasks.append(createNewTask);
+  }  
 
-  // moviesListz
+  moviesList.push({
+     id: `${globalTasks.children.length.toString()}`,
+     title: inp.value, status: false 
+    })
 
-  // moviesList.push({ id: "11", title: inp.value, status: false });
-
-  // localStorage.setItem("tasks", JSON.stringify(moviesList));
-  // console.log(moviesList);
+  localStorage.setItem("tasks", JSON.stringify(moviesList))
 
   inp.value = "";
 });
 
 
+// CRUD
 
-// function addDataToLocalStorage(data, lcKey) {}
+// CREATE
+// READ
+// UPDATE
+// DELETE
 
-// function removeDataFromLocalStorage(id, lcKey) {}
+// CREATE function addDataToLocalStorage(data, lcKey) {}
 
-// function changeDataInLocalStorage(id, data, lcKey) {}
+// DELETE function removeDataFromLocalStorage(id, lcKey) {}
 
-// // кнопки функции, LocalStorage
+// UPDATE function changeDataInLocalStorage(id, data, lcKey) {}
+
+// LocalStorage
 
 // //********************************* LocalStorage  */
 
@@ -202,49 +208,3 @@ circleButton.addEventListener("click", (e) => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const createNewTask1 = createTask("Сделать дз 1");
-// globalTasks.append(createNewTask1);
-// setNewTask();
-
-// const createNewTask2 = createTask("Сделать дз 2");
-// globalTasks.append(createNewTask2);
-// setNewTask();
-
-// const createNewTask3 = createTask("Сделать дз 3");
-// globalTasks.append(createNewTask3);
-// setNewTask();
-
-//(+)адекватная реакция на новые таски чтобы зачеркивались
-
-//(+)массив подгружается в данные тасок, исходит из них (используя цикл)
-//оптимизировать код с чат gpt
-
-
-
-// const todoList = document.querySelectorAll(".task");
-
-// const allButon = document.querySelector(".all");
-// const doneButon = document.querySelector(".done");
-// const notDoneButon = document.querySelector(".notDone");
